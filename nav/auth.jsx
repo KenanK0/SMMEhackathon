b10a5b8b2c093e94d4af4bc7cb1c321e7ff91591
kibambe <69826863+kibambe-0167@users.noreply.github.com>
2022-06-25 14:29:23 +0200

    merge errors

--- nav/auth.jsx
+++ nav/auth.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Text, View, ActivityIndicator, Pressable, Image } from "react-native";
+import { Text } from "react-native";
+import Register from "../screens/auth/register";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer } from "@react-navigation/native";
 
-import DefaultImage from "./icon.png";
+const stack = createNativeStackNavigator();
 
 const DEFAULT_IMAGE = Image.resolveAssetSource(DefaultImage).uri;
 
@@ -11,70 +11,9 @@ const Stack = createNativeStackNavigator();
 
 function Welcome({ navigation }) {
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "space-between",
-      }}
-    >
-      <View
-        style={{
-          borderWidth: 1,
-          width: "100%",
-          height: "80%",
-          borderBottomLeftRadius: 20,
-          borderBottomRightRadius: 20,
-          backgroundColor: "#1F376A",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          // flex: 2,
-        }}
-      >
-        <Image
-          source={{ uri: DEFAULT_IMAGE }}
-          style={{
-            width: 260,
-            height: 260,
-            resizeMode: "contain",
-          }}
-        />
-        <Text
-          style={{
-            color: "white",
-            fontSize: 60,
-          }}
-        >
-          PocketBA
-        </Text>
-        {/* <ActivityIndicator /> */}
-      </View>
-      <View
-        style={{
-          alignItems: "center",
-          justifyContent: "center",
-          height: "20%",
-        }}
-      >
-        <Pressable
-          style={{
-            backgroundColor: "#9A1206",
-            padding: 10,
-            width: 150,
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            borderBottomRightRadius: 8,
-            borderTopLeftRadius: 8,
-            elevation: 10,
-          }}
-          onPress={() => navigation.navigate("Login")}
-        >
-          <Text style={{ color: "white",fontSize:20 }}>Get started</Text>
-        </Pressable>
-      </View>
-    </View>
+    <stack.Navigator >
+      <stack.Screen name="Register" component={Register} />
+    </stack.Navigator>
   );
 }
 
